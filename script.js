bde822dc9e42f4cf2fe679ee2e3ae48655537ba0
Prakopchyk <prokopchike1987@gmail.com>
2023-04-30 18:08:28 +0200

    fix: handle hardware buttons press

--- script.js
+++ script.js
@@ -10,72 +10,80 @@ class Key {
   }
 }
 
-const kbKeys = [
-  new Key("`", "Backquote", "~"),
-  new Key("1", "Digit1", "!"),
-  new Key("2", "Digit2", "@"),
-  new Key("3", "Digit3", "#"),
-  new Key("4", "Digit4", "$"),
-  new Key("5", "Digit5", "%"),
-  new Key("6", "Digit6", "^"),
-  new Key("7", "Digit7", "&"),
-  new Key("8", "Digit8", "*"),
-  new Key("9", "Digit9", "("),
-  new Key("0", "Digit0", ")"),
-  new Key("-", "Minus", "_"),
-  new Key("=", "Equal", "+"),
-  new Key("Backspace", "Backspace", ""),
-  new Key("Tab", "Tab", ""),
-  new Key("q", "KeyQ", ""),
-  new Key("w", "KeyW", ""),
-  new Key("e", "KeyE", ""),
-  new Key("r", "KeyR", ""),
-  new Key("t", "KeyT", ""),
-  new Key("y", "KeyY", ""),
-  new Key("u", "KeyU", ""),
-  new Key("i", "KeyI", ""),
-  new Key("o", "KeyO", ""),
-  new Key("p", "KeyP", ""),
-  new Key("[", "BracketLeft", "{"),
-  new Key("]", "BracketRight", "}"),
-  new Key('\n\\', "Backslash", '\n|'),
-  new Key("CapsLock", "CapsLock", ""),
-  new Key("a", "KeyA", ""),
-  new Key("s", "KeyS", ""),
-  new Key("d", "KeyD", ""),
-  new Key("f", "KeyF", ""),
-  new Key("g", "KeyG", ""),
-  new Key("h", "KeyH", ""),
-  new Key("j", "KeyJ", ""),
-  new Key("k", "KeyK", ""),
-  new Key("l", "KeyL", ""),
-  new Key(";", "Semicolon", ":"),
-  new Key("'", "Quote", '"'),
-  new Key("Enter", "Enter", ""),
-  new Key("Shift", "ShiftLeft", ""),
-  new Key("z", "KeyZ", ""),
-  new Key("x", "KeyX", ""),
-  new Key("c", "KeyC", ""),
-  new Key("v", "KeyV", ""),
-  new Key("b", "KeyB", ""),
-  new Key("n", "KeyN", ""),
-  new Key("m", "KeyM", ""),
-  new Key("l", "KeyL", ""),
-  new Key(",", "Comma", "<"),
-  new Key(".", "Period", ">"),
-  new Key("/", "Slash", "?"),
-  new Key("↑", "ArrowUp", ""),
-  new Key("Shift", "ShiftRight", ""),
-  new Key("CTRL", "ControlLeft", ""),
-  new Key("Win", "MetaLeft", ""),
-  new Key("Alt", "AltLeft", ""),
-  new Key(" ", "Space", " "),
-  new Key("Alt", "AltRight", ""),
-  new Key("CTRL", "ControlRight", ""),
-  new Key("←", "ArrowLeft", ""),
-  new Key("↓", "ArrowDown", ""),
-  new Key("→", "ArrowRight", "")
-];
+const kbKeys =
+  [[
+    new Key("`", "Backquote", "~"),
+    new Key("1", "Digit1", "!"),
+    new Key("2", "Digit2", "@"),
+    new Key("3", "Digit3", "#"),
+    new Key("4", "Digit4", "$"),
+    new Key("5", "Digit5", "%"),
+    new Key("6", "Digit6", "^"),
+    new Key("7", "Digit7", "&"),
+    new Key("8", "Digit8", "*"),
+    new Key("9", "Digit9", "("),
+    new Key("0", "Digit0", ")"),
+    new Key("-", "Minus", "_"),
+    new Key("=", "Equal", "+"),
+    new Key("Backspace", "Backspace", "")
+  ],
+  [
+    new Key("Tab", "Tab", ""),
+    new Key("q", "KeyQ", ""),
+    new Key("w", "KeyW", ""),
+    new Key("e", "KeyE", ""),
+    new Key("r", "KeyR", ""),
+    new Key("t", "KeyT", ""),
+    new Key("y", "KeyY", ""),
+    new Key("u", "KeyU", ""),
+    new Key("i", "KeyI", ""),
+    new Key("o", "KeyO", ""),
+    new Key("p", "KeyP", ""),
+    new Key("[", "BracketLeft", "{"),
+    new Key("]", "BracketRight", "}"),
+    new Key('\\', "Backslash", '|')
+  ],
+  [
+    new Key("CapsLock", "CapsLock", ""),
+    new Key("a", "KeyA", ""),
+    new Key("s", "KeyS", ""),
+    new Key("d", "KeyD", ""),
+    new Key("f", "KeyF", ""),
+    new Key("g", "KeyG", ""),
+    new Key("h", "KeyH", ""),
+    new Key("j", "KeyJ", ""),
+    new Key("k", "KeyK", ""),
+    new Key("l", "KeyL", ""),
+    new Key(";", "Semicolon", ":"),
+    new Key("'", "Quote", '"'),
+    new Key("Enter", "Enter", "")
+  ],
+  [
+    new Key("Shift", "ShiftLeft", ""),
+    new Key("z", "KeyZ", ""),
+    new Key("x", "KeyX", ""),
+    new Key("c", "KeyC", ""),
+    new Key("v", "KeyV", ""),
+    new Key("b", "KeyB", ""),
+    new Key("n", "KeyN", ""),
+    new Key("m", "KeyM", ""),
+    new Key(",", "Comma", "<"),
+    new Key(".", "Period", ">"),
+    new Key("/", "Slash", "?"),
+    new Key("↑", "ArrowUp", ""),
+    new Key("Shift", "ShiftRight", "")
+  ],
+  [
+    new Key("CTRL", "ControlLeft", ""),
+    new Key("Win", "MetaLeft", ""),
+    new Key("Alt", "AltLeft", ""),
+    new Key(" ", "Space", " "),
+    new Key("Alt", "AltRight", ""),
+    new Key("CTRL", "ControlRight", ""),
+    new Key("←", "ArrowLeft", ""),
+    new Key("↓", "ArrowDown", ""),
+    new Key("→", "ArrowRight", "")
+  ]];
 
 // const keyBoardSymbols = ["`~", "!1", "@2", "#3", "$4", "%5", "^6", "&7", "*8", "(9", ")0", "-_", "+=", "Backspace",
 //   "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "DEL",
@@ -93,16 +101,19 @@ keyBoard.classList.add('keyboard');
 textArea.after(keyBoard);
 
 window.addEventListener('keydown', (e) => {
-  console.log(e)
+  // console.log(e)
   let button = getButton(e.code);
   if (button != null) {
     button.classList.add('pressed');
   }
+  textArea.value = textArea.value + e.key;
   e.preventDefault();
 })
 
+let pressedButtonCode = null;
+
 window.addEventListener('keyup', (e) => {
-  console.log(e)
+  // console.log(e)
   let button = getButton(e.code);
   if (button != null) {
     button.classList.remove('pressed');
@@ -120,8 +131,10 @@ function getButton(code) {
 function init() {
   let html = '';
   for (let i = 0; i < kbKeys.length; i++) {
-    let key = kbKeys[i];
-    html += `<div class="button" data-symbol="${key.symbol}" data-key="${key.code}" data-alt="${key.alt}"></div>`;
+    let row = kbKeys[i];
+    for (let j = 0; j < row.length; j++) {
+      html += `<button class="button" data-symbol="${row[j].symbol}" data-key="${row[j].code}" data-alt="${row[j].alt}"></button>`;
+    }
   }
   document.querySelector('.keyboard').innerHTML = html;
 
@@ -129,22 +142,29 @@ function init() {
 
   allButtons.forEach(button => {
     button.addEventListener("mousedown", function () {
-
       let code = button.getAttribute('data-key');
       let symbol = button.getAttribute('data-symbol');
-      const event = new KeyboardEvent("keydown", { bubbles: true, code: `${code}` });
+      pressedButtonCode = code;
 
+
+      let event = new KeyboardEvent("keydown", { bubbles: true, code: `${code}`, key: `${symbol}` });
+      // const event = new KeyboardEvent("keydown", { bubbles: true, cancelable: true, code: "KeyQ", key: "Q", char: "Q", shiftKey: true });
       textArea.dispatchEvent(event);
 
-      textArea.value = textArea.value + symbol;
-    })
 
-    button.addEventListener("mouseup", function () {
-      let code = button.getAttribute('data-key');
-      const event = new KeyboardEvent("keyup", { bubbles: true, code: `${code}` });
+
+    })
+  })
+
+  window.addEventListener("mouseup", function () {
+    console.log("mouseup")
+    if (pressedButtonCode !== null) {
+      console.log(pressedButtonCode)
+      let event = new KeyboardEvent("keyup", { bubbles: true, code: `${pressedButtonCode}` });
 
       textArea.dispatchEvent(event);
-    })
+    }
+    pressedButtonCode = null;
   })
 }
 
